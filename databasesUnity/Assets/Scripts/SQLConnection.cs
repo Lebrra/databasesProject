24982523f2bb9514d5e4654127e52b30bfb9c5a0
Leah Blasczyk <scout1665@gmail.com>
2021-04-13 09:57:30 -0500

    created unity project to connect with database

---
+++ databasesUnity/Assets/Scripts/SQLConnection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class SQLConnection
+{
+    MySqlConnection connection;
+
+    public void Connect()
+    {
+        connection = new MySqlConnection("");
+        
+    }
+}
