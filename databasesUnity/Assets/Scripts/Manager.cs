51be8b0f2f927b4aa37b5dd6bf5353fe83f7b7dd
Leah Blasczyk <scout1665@gmail.com>
2021-04-27 10:57:26 -0500

    can connect to database now

---
+++ databasesUnity/Assets/Scripts/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    void Start()
+    {
+        SQLConnection.Connect();
+
+        SQLConnection.testSQL();
+    }
+
+
+    private void OnApplicationQuit()
+    {
+        SQLConnection.EndConnection();
+    }
+}
--- databasesUnity/Assets/Scripts/SQLConnection.cs
+++ databasesUnity/Assets/Scripts/SQLConnection.cs
@@ -1,16 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MySql.Data;
+using System.Data;
 using MySql.Data.MySqlClient;
 
-public class SQLConnection
+public static class SQLConnection
 {
-    MySqlConnection connection;
+    static MySqlConnection connection = null;
 
-    public void Connect()
+	static string databasePrefix = "cs366-2211_blasczyklm05";
+	static string netID = "blasczyklm05";
+	static string hostName = "washington.uww.edu";
+	static string password = "lb1574";
+
+	public static void Connect()
     {
-        connection = new MySqlConnection("");
-        
-    }
+		try
+		{
+			string connectString = "Server=" + hostName + "; Database=" + databasePrefix + "; Uid=" + netID + "; Pwd=" + password + ";";
+			//string connectString = "Server=" + hostName + "; Database=" + databasePrefix + "; Uid=" + netID + "; Pwd=" + password + "; CharSet=utf8;";
+			Debug.Log(connectString);
+			connection = new MySqlConnection(connectString);
+			connection.Open();
+			Debug.Log("Successfully connected to the database");
+		}
+		catch (MySqlException ex)
+		{
+			Debug.LogWarning("Unsuccessful connection.");
+			Debug.LogWarning(ex.Message);
+		}
+	}
+
+	public static void testSQL()
+	{
+		try
+		{
+			MySqlCommand facultyCountCall = new MySqlCommand();
+			facultyCountCall.Connection = connection;
+			facultyCountCall.CommandType = CommandType.StoredProcedure;
+			facultyCountCall.CommandText = "getTotalFaculty";
+
+			facultyCountCall.Parameters.Add("@total", MySqlDbType.Int32);
+			facultyCountCall.Parameters["@total"].Direction = ParameterDirection.InputOutput;
+
+			facultyCountCall.ExecuteNonQuery();
+
+			Debug.Log("Total Faculty: " + facultyCountCall.Parameters["@total"].Value);
+		}
+		catch (MySqlException ex)
+		{
+			Debug.LogWarning(ex.ToString());
+		}
+	}
+
+	public static void EndConnection()
+    {
+		connection?.Close();
+		Debug.Log("Closed connection.");
+	}
 }
