8011219c6b53b03b4731113e5d54d4530bcbcbd3
Leah Blasczyk <scout1665@gmail.com>
2021-05-01 14:51:03 -0500

    added hover colors to custom buttons

--- databasesUnity/Assets/Scripts/DevButton.cs
+++ databasesUnity/Assets/Scripts/DevButton.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DevButton : MonoBehaviour, IPointerClickHandler
+public class DevButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public int devID = -1;
+
+    Color32 defaultText = new Color32(30, 72, 149, 255);
+    Color32 hoverText = new Color32(89, 210, 231, 255);
+    Color32 clickText = new Color32(241, 241, 241, 255);
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (devID > 0)
         {
+            if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = clickText;
+
             DataUIPooler.poolInstance.GetDevPanel(devID);
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = hoverText;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = defaultText;
+    }
+
+    private void OnEnable()
+    {
+        if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = defaultText;
     }
 }
--- databasesUnity/Assets/Scripts/GameButton.cs
+++ databasesUnity/Assets/Scripts/GameButton.cs
@@ -3,15 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class GameButton : MonoBehaviour, IPointerClickHandler
+public class GameButton : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public int gameRank = -1;
+
+    Color32 defaultText = new Color32(30, 72, 149, 255);
+    Color32 hoverText = new Color32(89, 210, 231, 255);
+    Color32 clickText = new Color32(241, 241, 241, 255);
+
+    Color defaultImg = Color.white;
+    Color32 hoverImg = new Color32(215, 255, 189, 255);
+    Color32 clickImg = new Color32(176, 243, 130, 255);
 
     public void OnPointerClick(PointerEventData eventData)
     {
         if (gameRank > 0)
         {
+            if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = clickText;
+            if (GetComponent<UnityEngine.UI.RawImage>()) GetComponent<UnityEngine.UI.RawImage>().color = clickImg;
+
             DataUIPooler.poolInstance.GetGamePanel(gameRank);
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = hoverText;
+        if (GetComponent<UnityEngine.UI.RawImage>()) GetComponent<UnityEngine.UI.RawImage>().color = hoverImg;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = defaultText;
+        if (GetComponent<UnityEngine.UI.RawImage>()) GetComponent<UnityEngine.UI.RawImage>().color = defaultImg;
+    }
+
+    private void OnEnable()
+    {
+        if (GetComponent<TMPro.TextMeshProUGUI>()) GetComponent<TMPro.TextMeshProUGUI>().color = defaultText;
+        if (GetComponent<UnityEngine.UI.RawImage>()) GetComponent<UnityEngine.UI.RawImage>().color = defaultImg;
     }
 }
